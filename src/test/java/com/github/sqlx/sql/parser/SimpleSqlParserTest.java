53fdc9b64db00238949fe88a40bfc166febcdd70
HP <anonymou_s@163.com>
2025-04-07 21:31:34 +0800

    sts for SimpleSqlParser

--- src/test/java/com/github/sqlx/sql/parser/SimpleSqlParserTest.java
+++ src/test/java/com/github/sqlx/sql/parser/SimpleSqlParserTest.java
@@ -4,23 +4,22 @@ import com.github.sqlx.sql.SqlAttribute;
 import com.github.sqlx.sql.SqlType;
 import org.junit.jupiter.api.Test;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * @author He Xing Mo
- * @since 1.0
- *
+ * Unit tests for SimpleSqlParser.
+ * This class tests the parsing of different SQL statements to ensure
+ * correct identification of SQL type and read/write status.
+ * 
  * @see SimpleSqlParser
  */
 class SimpleSqlParserTest {
 
     static SqlParser sqlParser = new SimpleSqlParser();
 
-
     @Test
     void parseSelectTest() {
-        String sql = " select * from students ";
+        String sql = "select * from students ";
         SqlAttribute sqlAttribute = sqlParser.parse(sql);
         assertThat(sqlAttribute).isNotNull();
         assertThat(sqlAttribute.isWrite()).isFalse();
@@ -59,5 +58,15 @@ class SimpleSqlParserTest {
         assertThat(sqlAttribute.isWrite()).isTrue();
         assertThat(sqlAttribute.isRead()).isFalse();
         assertThat(sqlAttribute.getSqlType()).isEqualTo(SqlType.DELETE);
+    }
+
+    @Test
+    void parseOtherSqlTest() {
+        String sql = "CREATE TABLE students (id INT, name VARCHAR(100));";
+        SqlAttribute sqlAttribute = sqlParser.parse(sql);
+        assertThat(sqlAttribute).isNotNull();
+        assertThat(sqlAttribute.isWrite()).isFalse();
+        assertThat(sqlAttribute.isRead()).isFalse();
+        assertThat(sqlAttribute.getSqlType()).isEqualTo(SqlType.OTHER);
     }
 }
